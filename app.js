6fb46ff5660a7c79e0498206eb804ab796ebfa0b
Esotastic <chrishjimenez@gmail.com>
2018-04-17 21:43:00 -0400

    Set up API test
    Added in hard-coded API test to verify functioning routes

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ var app = express();
 var request = require("request");
 
 app.get("/results", function(req, res){
-  reuqees.send("It works.");
+  request("http://www.omdbapi.com/?s=california&apikey=thewdb", function(error, response, body){
+    if(!error && response.statusCode == 200){
+      var resultsParsed = JSON.parse(body);
+      res.send(results["Search"][0]);
+    }
+  });
 });
 
 app.listen(3000, "localhost", function(){
